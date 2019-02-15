bd669706c0f6c4a6b95a42a4783851f33fdd79f1
François Hodierne <francois@hodierne.net>
2019-02-15 17:48:37 +0100

    ci: simpler CI setup for API

--- scripts/setup_circleci.sh
+++ scripts/setup_circleci.sh
@@ -1,64 +1,29 @@
 #!/bin/bash
-# This script only runs on circleci, just before the e2e tests
-# first version cfr. https://discuss.circleci.com/t/add-ability-to-cache-apt-get-programs/598/6
-
-if [ "$NODE_ENV" = "circleci" ]; then
-  echo "Performing circleci e2e setup because NODE_ENV is '${NODE_ENV}'";
-else
-  echo "Skipping circleci e2e setup because NODE_ENV is '${NODE_ENV}'";
-  exit;
-fi
 
 mkdir -p ~/cache
+
 cd ~/cache
 
-API_TARBALL_URL="https://codeload.github.com/opencollective/opencollective-api/tar.gz/";
-if curl -s --head  --request GET "${API_TARBALL_URL}${CIRCLE_BRANCH}" | grep "200" > /dev/null
-then
-  BRANCH=$CIRCLE_BRANCH;
+if [ -d "opencollective-api/.git" ]; then
+  cd opencollective-api
 else
-  BRANCH="master";
+  rm -rf opencollective-api
+  git clone https://github.com/opencollective/opencollective-api.git
+  cd opencollective-api
 fi
 
-# If we already have an archive of the branch locally (in ~/cache)
-# Then we check to see if the size matches the online version
-# If they do, we proceed to start the api server
-# Otherwise we remove the local cache and install latest version of the branch
+echo "> Matching frontend branch if it exists"
+git checkout ${CIRCLE_BRANCH} 2>/dev/null
 
-TARBALL_SIZE=$(curl -s --head  --request GET "${API_TARBALL_URL}${BRANCH}" | grep "Content-Length" | sed -E "s/.*: *([0-9]+).*/\1/")
+echo "> Pulling latest changes from GitHub"
+git pull origin
 
-if [ ! $TARBALL_SIZE ]; then
-  # First request doesn't always provide the content length for some reason (it's probably added by their caching layer)
-  TARBALL_SIZE=$(curl -s --head  --request GET "${API_TARBALL_URL}${BRANCH}" | grep "Content-Length" | sed -E "s/.*: *([0-9]+).*/\1/")
-fi
+echo "> Installing npm packages"
+npm ci
 
-ARCHIVE="${BRANCH//\//-}.tgz"
+echo "> Building API"
+npm run build
 
-if [ -e $ARCHIVE ];
-then
-  LSIZE=$(wc -c $ARCHIVE | sed -E "s/ ?([0-9]+).*/\1/")
-  test $TARBALL_SIZE = $LSIZE && echo "Size matches $ARCHIVE (${TARBALL_SIZE}:${LSIZE})" || (echo "> Removing old $ARCHIVE (size doesn't match: ${TARBALL_SIZE}:${LSIZE})"; rm $ARCHIVE; echo "File removed";)
-fi
-
-if [ ! -e $ARCHIVE ];
-then
-  echo "> Downloading tarball ${API_TARBALL_URL}${BRANCH}"
-  curl  "${API_TARBALL_URL}${BRANCH}" -o $ARCHIVE
-  echo "> Extracting $ARCHIVE"
-  tar -xzf $ARCHIVE
-  if [ -d "opencollective-api" ]; then
-    rm -rf opencollective-api
-  fi
-  mv "opencollective-api-${BRANCH//\//-}" opencollective-api
-  cd "opencollective-api"
-  echo "> Running npm install for api"
-  npm ci
-  echo "> Running build for api"
-  npm run build
-  cd ..
-fi
-
-cd "opencollective-api"
 echo "> Restoring opencollective_dvl database for e2e testing";
 export PGPORT=5432
 export PGHOST=localhost
